93d15c480276fe629e1ce51e0b8644465e26c802
Andrew Plummer <plummer.andrew@gmail.com>
2021-08-16 17:56:33 +0900

    fixes for generated components having lint issues

--- src/generate/screens.js
+++ src/generate/screens.js
@@ -12,6 +12,7 @@ const SCREENS_DIR = 'services/web/src/screens';
 
 export async function generateScreens(options) {
   const { pluralUpper } = options;
+  options.overviewImports = {};
 
   const screensDir = await assertScreensDir();
 
@@ -19,9 +20,9 @@ export async function generateScreens(options) {
   for (let file of FILES) {
     let source = await readSourceFile(screensDir, 'Shops', file);
     source = replacePrimary(source, options);
-    source = replaceSubScreenImports(source, options);
     source = replaceSubScreenRoutes(source, options);
     source = replaceSubScreenMenus(source, options);
+    source = replaceDetailImports(source, options);
     source = replaceOverviewFields(source, options);
     source = replaceOverviewRows(source, options);
     source = replaceOverviewImports(source, options);
@@ -59,6 +60,7 @@ async function generateSubScreensFor(screensDir, source, primary, secondary) {
               src = replaceSecondary(src, secondary);
               src = replaceListHeaderCells(src, secondary);
               src = replaceListBodyCells(src, secondary, primary);
+              src = replaceListImports(src, primary);
               await writeLocalFile(src, screensDir, primary.pluralUpper, 'Detail', `${secondary.pluralUpper}.js`);
             });
           })
@@ -66,18 +68,6 @@ async function generateSubScreensFor(screensDir, source, primary, secondary) {
       })
     );
   }
-}
-
-function replaceSubScreenImports(source, options) {
-  const { subScreens = [] } = options;
-  const imports = subScreens
-    .map((resource) => {
-      const { pluralUpper } = resource;
-      return `import ${pluralUpper} from './${pluralUpper}';`;
-    })
-    .join('\n');
-
-  return replaceBlock(source, imports, 'imports');
 }
 
 function replaceSubScreenRoutes(source, options) {
@@ -119,6 +109,18 @@ function replaceSubScreenMenus(source, options) {
   return replaceBlock(source, imports, 'menus');
 }
 
+function replaceDetailImports(source, options) {
+  const { subScreens = [] } = options;
+  const imports = subScreens
+    .map((resource) => {
+      const { pluralUpper } = resource;
+      return `import ${pluralUpper} from './${pluralUpper}';`;
+    })
+    .join('\n');
+
+  return replaceBlock(source, imports, 'detail-imports');
+}
+
 function replaceOverviewFields(source, options) {
   const { camelLower } = options;
   const summaryFields = getSummaryFields(options);
@@ -127,12 +129,14 @@ function replaceOverviewFields(source, options) {
     .map((field) => {
       const { name } = field;
       if (name === 'image') {
+        options.overviewImports.image = true;
         return block`
         {${camelLower}.image && (
           <Image key={${camelLower}.image.id} src={urlForUpload(${camelLower}.image)} />
         )}
       `;
       } else if (name === 'images') {
+        options.overviewImports.image = true;
         return block`
         <Image.Group size="small">
           {${camelLower}.images.map((image) => (
@@ -141,6 +145,7 @@ function replaceOverviewFields(source, options) {
         </Image.Group>
       `;
       } else {
+        options.overviewImports.header = true;
         return block`
         <Header as="h3">{${camelLower}.${name}}</Header>
       `;
@@ -165,7 +170,7 @@ function replaceOverviewRows(source, options) {
         <Table.Row>
           <Table.Cell>${startCase(name)}</Table.Cell>
           <Table.Cell>
-            {${getOverviewCellValue(`${options.camelLower}.${name}`, field)}}
+            {${getOverviewCellValue(`${options.camelLower}.${name}`, field, options)}}
           </Table.Cell>
         </Table.Row>
       `;
@@ -178,7 +183,7 @@ function replaceOverviewRows(source, options) {
   return source;
 }
 
-function getOverviewCellValue(token, field) {
+function getOverviewCellValue(token, field, options) {
   switch (field.type) {
     case 'UploadArray':
     case 'StringArray':
@@ -188,8 +193,10 @@ function getOverviewCellValue(token, field) {
       return `${token} ? 'Yes' : 'No'`;
     case 'Date':
       if (field.time) {
+        options.overviewImports.time = true;
         return `${token} ? formatDateTime(${token}) : 'None'`;
       } else {
+        options.overviewImports.date = true;
         return `${token} ? formatDate(${token}) : 'None'`;
       }
     default:
@@ -198,19 +205,17 @@ function getOverviewCellValue(token, field) {
 }
 
 function replaceOverviewImports(source, options) {
-  const { schema } = options;
-
   const imports = [];
 
   const dateMethods = ['formatDateTime'];
 
-  if (schema.some((field) => field.type === 'Date' && !field.time)) {
-    dateMethods.push('formatDate');
+  if (options.overviewImports.date) {
+    dateMethods.unshift('formatDate');
   }
 
   imports.push(`import { ${dateMethods.join(', ')} } from 'utils/date';`);
 
-  if (schema.some((field) => field.type.match(/Upload/))) {
+  if (options.overviewImports.image) {
     imports.push("import { urlForUpload } from 'utils/uploads';");
   }
 
@@ -246,11 +251,13 @@ function replaceListHeaderCells(source, options) {
 }
 
 function replaceListBodyCells(source, options, resource) {
-  let link = false;
-  if (!resource) {
+  let isSubscreen = !!resource;
+  if (!isSubscreen) {
     resource = options;
-    link = true;
   }
+  resource.listImports = {
+    type: isSubscreen ? 'subscreen-imports' : 'list-imports',
+  };
 
   const { camelLower, pluralKebab } = resource;
 
@@ -261,6 +268,7 @@ function replaceListBodyCells(source, options, resource) {
       let inner;
       if (name === 'image' || name === 'images') {
         const idx = name === 'images' ? '[0]' : '';
+        resource.listImports.image = true;
         inner = `
         {${camelLower}.${name}${idx} && (
           <Image size="tiny" src={urlForUpload(${camelLower}.${name}${idx}, true)} />
@@ -270,7 +278,8 @@ function replaceListBodyCells(source, options, resource) {
         inner = `{${camelLower}.${name}}`;
       }
 
-      if (i === 0 && link) {
+      if (i === 0 && !isSubscreen) {
+        resource.listImports.link = true;
         inner = `
           <Link to={\`/${pluralKebab}/\${${camelLower}.id}\`}>
             ${inner}
@@ -289,19 +298,19 @@ function replaceListBodyCells(source, options, resource) {
 }
 
 function replaceListImports(source, options) {
-  const { schema } = options;
-
   const imports = [];
+  const { listImports } = options;
 
-  if (schema.some((field) => field.type.match(/Upload/))) {
+  if (listImports.image) {
     imports.push("import { urlForUpload } from 'utils/uploads';");
-  }
-
-  if (schema.some(({ name }) => name === 'image' || name === 'images')) {
     imports.push("import { Image } from 'semantic-ui-react';");
   }
 
-  source = replaceBlock(source, imports.join('\n'), 'list-imports');
+  if (listImports.link) {
+    imports.push("import { Link } from 'react-router-dom';");
+  }
+
+  source = replaceBlock(source, imports.join('\n'), listImports.type);
 
   return source;
 }

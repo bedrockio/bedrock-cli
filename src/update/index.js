00c5bbd5bb9951f3c48ff764ebffbd34a36e98e0
Andrew Plummer <plummer.andrew@gmail.com>
2022-02-04 02:04:34 +0900

    fix for checking package manager

--- src/update/index.js
+++ src/update/index.js
@@ -5,8 +5,29 @@ import { exec, withDir } from '../util/shell';
 
 export default async function update() {
   await withDir(path.resolve(homedir(), '.bedrock'), async () => {
+    const manager = await getPackageManager();
     await exec(`git pull`);
-    await exec(`yarn install`);
+    await exec(`${manager} install`);
   });
   console.log(kleur.green('Updated!'));
+}
+
+async function getPackageManager() {
+  let manager = await tryPackageManager('yarn');
+  if (!manager) {
+    manager = await tryPackageManager('npm');
+  }
+  if (!manager) {
+    throw new Error('Either yarn or npm must be installed!');
+  }
+  return manager;
+}
+
+async function tryPackageManager(name) {
+  try {
+    await exec(`command -v ${name}`);
+    return name;
+  } catch (error) {
+    return null;
+  }
 }

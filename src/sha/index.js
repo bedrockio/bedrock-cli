1617a0398f502adc9b6d42cd66df99bc24dbb475
Kaare Larsen <me@kaareal.com>
2024-07-17 11:24:48 +0200

    fixing

--- src/sha/index.js
+++ src/sha/index.js
@@ -4,8 +4,10 @@ import { homedir } from 'os';
 import { exec, withDir } from '../util/shell';
 
 export default async function sha() {
+  let gitSha; 
   await withDir(path.resolve(homedir(), '.bedrock'), async () => {
-    const gitSha = await exec(`git rev-parse --short HEAD`);
+    gitSha = await exec(`git rev-parse --short HEAD`);
     console.log(kleur.green(`Current git commit SHA: `)+kleur.yellow(gitSha));
   });
+  return gitSha;
 }
